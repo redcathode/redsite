20495ce8bc97e458372e2bda73cb0b7633289648
red cathode <max@b0vik.dev>
2025-07-08 17:41:22 -0400

    Update portfolio

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -1,11 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const blog = await getCollection('blog');
+  const blog = await getCollection("blog");
   return rss({
-    title: 'red blog',
-    description: 'scattered, inane ramblings',
+    title: "red blog",
+    description: "max's ramblings",
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
