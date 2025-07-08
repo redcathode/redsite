81892d2a7d48c253047c156ab7a646ba0c382ecd
red cathode <max@b0vik.dev>
2025-07-08 12:38:27 -0400

    add mathjax rendering to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
 
 import mdx from "@astrojs/mdx";
+import rehypeMathJaxSvg from "rehype-mathjax/svg";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
-      theme: 'dracula',
-      wrap: true
+      theme: "dracula",
+      wrap: true,
     },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathJaxSvg],
   },
-  site: 'https://redcatho.de'
+  site: "https://redcatho.de",
 });
